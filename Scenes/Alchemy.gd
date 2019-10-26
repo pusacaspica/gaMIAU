bbd821bb972a30d911a7366c1f88b652ce1d87c8
lisahtoledo <elisartoledo@gmail.com>
2019-10-26 12:26:15 -0300

    Botões de adicionar cartas aos slots funcionando

--- Scenes/Alchemy.gd
+++ Scenes/Alchemy.gd
@@ -3,20 +3,44 @@ extends Node
 onready var recipes : Dictionary = PotionsDB.Potions
 onready var cards : Dictionary = CardsDB.Cards
 
-var slots : Array
+var slots : Dictionary
 var slots_amount = 4
-var occupied_slots = 0
 
 var inventory : Dictionary = {
 	"CardTest" : 4,
-	"CardTest2:" : 4
+	"CardTest2" : 4
 }
 
+func _ready():
+	_update_inventory()
+
+func _update_inventory():
+	var label_1 : Label = $Button_Type1/Amount_Label 
+	var label_2 : Label = $Button_Type2/Amount_Label 
+	
+	label_1.text = "Quantidade : " + str(inventory["CardTest"])
+	label_2.text = "Quantidade : " + str(inventory["CardTest2"])
+	
 func add_to_slots(id):
+	var occupied_slots = $Slots.get_child_count()
 	if occupied_slots < slots_amount:
-		occupied_slots += 1
-		var current_card = cards[id]
-		slots.append(current_card)
+		var current_card = Label.new()
+		current_card.text = id
+		$Slots.add_child(current_card)
+		inventory[id] -= 1
+		_update_inventory()
 	else:
 		pass
 
+func _on_Button_Type1_pressed():
+	add_to_slots("CardTest")
+	
+
+
+func _on_Button_Type2_pressed():
+	add_to_slots("CardTest2")
+
+
+
+func _on_Button_Mix_pressed():
+	pass # Replace with function body.
