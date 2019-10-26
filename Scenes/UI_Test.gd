a5763f49a7c86637cb0ff712eecf825eeb2ec32e
Diogo Amorim <diogoamorim27@gmail.com>
2019-10-26 11:42:58 -0300

    adding inv slots scripts

--- Scenes/UI_Test.gd
+++ Scenes/UI_Test.gd
@@ -1,9 +1,5 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-#onready var CardContainer : = $HBoxContainer
 onready var CardBase = preload("res://Scenes/CardBase.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
