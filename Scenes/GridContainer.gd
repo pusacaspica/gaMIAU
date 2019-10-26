77afedb781907c1ce287e7b26194f3504a65b0ad
Diogo Amorim <diogoamorim27@gmail.com>
2019-10-26 17:36:43 -0300

    comiitaa

--- Scenes/GridContainer.gd
+++ Scenes/GridContainer.gd
@@ -11,11 +11,15 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
 		#var cursor_pos = get_global_mouse_position()
-		for child in get_children():
-			if child.get_global_rect().has_point(get_global_mouse_position()) and get_parent().get_global_rect().has_point(get_global_mouse_position()):
-				if child.visible == true:
-					print("Haaa")
-					print (child)
-					child.visible = false
-					get_node("../..").pickup_item("CardTest")
+		if Input.is_action_just_pressed("inv_grab"):
+			for child in get_children():
+				if child.get_global_rect().has_point(get_global_mouse_position()) and get_parent().get_global_rect().has_point(get_global_mouse_position()):
+					if child.texture != null:
+						var slots_full = true
+						for slot in get_node("../..").slots:
+							if !slot.item:
+								slots_full = false 
+						if !slots_full:
+							child.texture = null
+							get_node("../..").pickup_item("CardTest")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
