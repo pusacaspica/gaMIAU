a1bbe31e269e4dd843fe7008a1e1eb97d0334419
Diogo Amorim <diogoamorim27@gmail.com>
2019-10-27 04:38:05 -0300

    ui test clientorder removed

--- Scenes/UI_Test.gd
+++ Scenes/UI_Test.gd
@@ -1,15 +1,19 @@
 extends Control
 
 onready var CardBase = preload("res://Scenes/CardBase.tscn")
+onready var ClientDialog = $Cliente/Panel/Label
+onready var Clients = ClientDB.Level1
 
-var ClientOrder = [
-	{ "Potion" : "happy_potion", "Fala" : "Blablabla" }
-]
-
+var current_client
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize()
+	current_client = Clients[randi() % Clients.size()]
+	ClientDialog.text = current_client["Fala"]
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$Moedas/Label.text = str(CurrentInvent.money)
+	
+
 
